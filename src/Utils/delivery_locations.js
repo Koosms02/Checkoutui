68cd400ea056122ba4a6a9fb201f4bbba8296d1c
Koosms02 <Koosms02@gmail.com>
2023-11-25 11:58:40 +0200

    travel_time added

---
+++ src/Utils/delivery_locations.js
@@ -0,0 +1,17 @@
+const location = [
+    { "main area": "PTA 1", "locations": ["Centurion", "Irene", "MidStream", "Sinoville", "Montana", "Brooklyn", "Hatfield", " Pretoria East"], "price": "100" },
+    { "main area": "PTA 2", "locations": ["Shere", "Silver Lakes", "Olifantsfontein", "Pretoria North", "Roodeplaat", "Culinan"], "price": "120" },
+    { "main area": "ESR1", "locations": ["Benoni", "Boksburg", "Kempton park", "Edenvale", "Bedfordview"], "price": "150" },
+    { "main area": "ESR2", "locations": ["Springs", "Nigel", "Germiston", "Modderfontein", "Brakpan"], "price": "180" },
+    { "main area": "JHB1", "locations": ["Sandton", "Fourways", "Kayalami", "Morningside", "Rivonia"], "price": "150" },
+    { "main area": "JHB2", "locations": ["Parktown", "Randburg", "CBD", "Mellville", "Rosebank", "Parkview", "Braamfontein", "surrounding"], "price": "180" },
+    { "main area": "JHB3", "locations": ["Roodepoort", "Alberton", "Surrounding"], "price": "200" },
+    { "main area": "Nation Wide", "locations": ["Outside Gauteng", "Krugersdorp", "Soweto", "Hartebeespoort", "Vereeniging", "Randfontein"], "price": "200+" },
+]
+
+const delivery_rules = [
+    "No EFT orders will be sent out until POP is sent via a screenshot on whtsapp stating that the payment was made immediate.",
+    "Drivers do not carry change on them for safety reasons,clients cannot request cash change.",
+    "DAW Drivers waiting time for a client is max 10 Minutes , Please ensure clients are ready to recieve their parcels.",
+    "DAW will accept orders up until 10: 20am for the morning route & 16: 10 for the afternoon route."
+]
--- src/js/Pages/items.js
+++ src/js/Pages/items.js
@@ -5,14 +5,17 @@ export function Items() {
 
     const w = currentWindowSize().innerWidth;
     return (
-        w > 1025 && <div className='flex flex-start flex-col h-screen pl-10 w-1/2 bg-white  '>
+        w > 1025 && <div className='flex flex-start justify-between flex-col h-screen pl-10 w-1/2 bg-white  '>
             {/* left */}
             <div className=' flex w-full h-16 items-center '>
-                <h3 className='text-black'>Items</h3>
+                <h3 className='text-black '>Items</h3>
             </div>
 
-            <div className=' flex w-full h-screen pt-16 '>
-                <h3 className='text-white'>Who is checking out?</h3>
+
+            <div className="h-full max-h-32">
+                <p className="text-black"> Sub Total:</p>
+                <p className="text-black"> Total:</p>
+
             </div>
         </div>);
 }
