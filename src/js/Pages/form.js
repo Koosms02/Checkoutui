6fe1220c6cfcc2eab3b6b5bfae40bcb236247d5f
Koosms02 <Koosms02@gmail.com>
2023-12-01 15:35:10 +0200

    saved the data

--- src/js/Pages/form.js
+++ src/js/Pages/form.js
@@ -7,21 +7,24 @@ export function Form() {
     const w = currentWindowSize().innerWidth
     console.log(w)
     const { register, handleSubmit } = useForm()
-    const [movePage, setMovePage] = useState({
-        "customer": true,
-        "shipping": false,
-        "payments": false
-    })
+    // const [movePage, setMovePage] = useState({
+    //     "customer": true,
+    //     "shipping": false,
+    //     "payments": false
+    // })
 
 
     const [paymentChoice, setPaymentChoice] = useState({ "cash": false, "online": true })
     const handleRegistration = (data) => console.log(data)
 
     return (
-
         <div className='flex flex-start flex-col h-screen pl-10 overflow-y-auto w-full bg-black  '>
             {w < 921 && <div className=' flex w-full h-16 items-center '>
                 <h3 className='text-white'>DAW Dispensary</h3>
+            </div>}
+
+            {w > 921 && <div className=' flex w-full h-16 items-center '>
+                {/* <h3 className='text-white'>DAW Dispensary</h3> */}
             </div>}
 
             {/*  users information */}
@@ -82,7 +85,6 @@ export function Form() {
                             <span className="ml-2">
                                 <p1 className="text-white">
                                     Pay with card
-
                                 </p1>
                             </span>
                         </label>
@@ -105,174 +107,18 @@ export function Form() {
                         </label>
                     </div>
 
-                    <div type="submit" onClick={() => {
-                        setMovePage({ "customer": false, "shipping": true, "payments": false })
-                        // setMovePage({ "customer": false, "payments": true })
-                        console.log(movePage)
-                    }} className="hover:bg-red-400 cursor-pointer flex h-12 w-44 justify-center items-center bg-white ">
-                        <h3>Proceed </h3>
-                    </div>
-
-                </form>
-
-            </div>
-
-
-
-            {/* Proceeding to the order  */}
-
-
-            {/* 
-            {movePage.shipping == true && <div className=' flex w-full h-screen pt-16 flex-col'>
-                <h3 className='text-white'>Where would you like the order be sent?</h3>
-
-                <form onSubmit={handleSubmit(handleRegistration)}>
-                    <div className="h-10" />
-                    <div className="flex flex-col w-full pr-10 h-16 max-w-md" >
-                        <label className="text-white">Country</label>
-                        <div className="h-2"></div>
-                        <input name="name " {...register('country')} />
-                    </div>
-
-                    <div className="flex flex-col w-full pr-10 max-w-md h-16 " >
-                        <label className="text-white">Address</label>
-                        <div className="h-2"></div>
-                        <input name="name " {...register('Address')} />
-                    </div>
-
-                    <div className="h-4" />
-                    <div className="flex flex-row w-full justify-between pr-10 max-w-md h-16" >
-                        <div className="pr-10">
-                            <label className="text-white">City</label>
-                            <div className="h-2"></div>
-                            <input name="name " {...register('City')} />
-                        </div>
-
-                        <div>
-                            <label className="text-white">Zip Code</label>
-                            <div className="h-2"></div>
-                            <input name="name " {...register('Zip Code')} />
-                        </div>
-
-
-                    </div>
-
-                    <div className="h-4" />
-                    <div className="flex flex-col w-full max-w-md  pr-10 h-16 " >
-                        <label className="text-white">Phone number*</label>
-                        <div className="h-2"></div>
-                        <input name="name " {...register('Address')} />
-                    </div>
-
-
-                    <div className="h-10" />
-
-                    <div type="submit" onClick={() => {
-                        setMovePage({ "customer": false, "shipping": false, "payments": true })
-                    }} className="hover:bg-red-400 cursor-pointer flex h-12 w-44 justify-center items-center bg-white ">
-                        <h3>Proceed to payments</h3>
-                    </div>
-
-
-
-                </form>
-
-            </div>} */}
-
-
-
-            {/* payment details */}
-
-
-            {movePage.payments == true && < div className=' flex w-full h-screen pt-16 flex-col'>
-                <h3 className='text-white'>How would you like to pay?</h3>
-
-                <div className="h-10" />
-                <div className="flex flex-col h-16  w-1/2">
-
-                    <label>
-                        <input
-                            type="radio"
-                            value="online"
-                            checked={paymentChoice.online}
-                            onChange={() => { setPaymentChoice({ "cash": false, "online": true }) }}
-                            className="pr-2"
-                        />
-                        <span className="ml-2">
-                            <p1 className="text-white">
-                                Pay with card
-
-                            </p1>
-                        </span>
-                    </label>
-
-
-                    <label>
-                        <input
-                            type="radio"
-                            value="cash"
-                            checked={paymentChoice.cash}
-                            //   onChange={handleOptionChange}
-                            onChange={() => { setPaymentChoice({ "cash": true, "online": false }) }}
-                        />
-                        {/* <div className="w-4" /> */}
-                        <span className="ml-2">
-                            <p1 className="text-white">
-                                Pay cash
-                            </p1>
-                        </span>
-                    </label>
-                </div>
-                {/* 
-                {paymentChoice.cash !== true && <form onSubmit={handleSubmit(handleRegistration)}>
-                    <div className="h-4" />
-                    <div className="flex flex-col w-full pr-10 max-w-md h-16 " >
-                        <label className="text-white">Name on card</label>
-                        <div className="h-2"></div>
-                        <input name="name " {...register('country')} />
-                    </div>
-                    <div className="h-4" />
-                    <div className="flex flex-col w-full h-16 pr-10 max-w-md " >
-                        <label className="text-white">card number</label>
-                        <div className="h-2"></div>
-                        <input name="name " {...register('Address')} />
-                    </div>
-
-                    <div className="h-4" />
-                    <div className="flex flex-row w-full pr-10 max-w-md h-16" >
-                        <div className="pr-10">
-                            <label className="text-white">Expiration</label>
-                            <div className="h-2"></div>
-                            <input name="name " {...register('City')} />
-                        </div>
-
-                        <div>
-                            <label className="text-white">CVVs</label>
-                            <div className="h-2"></div>
-                            <input name="name " {...register('Zip Code')} />
-                        </div>
-
-
-                    </div>
-
-
-
-
-                    <div className="h-10" />
-
                     <div type="submit" className="hover:bg-red-400 cursor-pointer flex h-12 w-44 justify-center items-center bg-white ">
-                        <h3>make payments</h3>
+                        <button type="submit">submit</button>
+                        {/* <h3>Proceed </h3> */}
                     </div>
 
+                </form>
 
-
-                </form>} */}
-
-                {/* {paymentChoice.cash === true && <div type="submit" className="hover:bg-red-400 cursor-pointer flex h-12 w-44 justify-center items-center bg-white ">
-                    <h3>Continue</h3>
-                </div>} */}
             </div>
-            }
+
+
+
+
         </div >
     );
 }
--- src/js/Pages/header.js
+++ src/js/Pages/header.js
@@ -9,7 +9,7 @@ function header() {
         w > 921 && <div className='flex flex-start flex-col h-screen pl-10 w-1/2 bg-black  '>
             {/* left */}
             <div className=' flex w-full h-16 items-center '>
-                <h3 className='text-white'>DAW</h3>
+                <h3 className='text-white'>DAW Dispensary</h3>
             </div>
 
             <div className=' flex w-full h-screen pt-16  '>
